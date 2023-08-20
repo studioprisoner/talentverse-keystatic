8be6477a525bf18358719af8e332e81b78f45fad
Josh Illichmann <josh@studioprisoner.com>
2023-08-20 23:04:07 +1000

    add fathom

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,7 @@
 import type { AppProps } from "next/app";
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import * as Fathom from 'fathom-client';
 
 import "../styles/global.css";
 import "../styles/scoped-preflight.css";
@@ -6,7 +9,29 @@ import "../styles/scoped-preflight.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
+
+
 function MyApp({ Component, pageProps }: AppProps) {
+
+  const router = useRouter();
+
+  useEffect(() => {
+    Fathom.load(process.env.FATHOM_ID, {
+      includedDomains: ['talentverse.com.au', 'talentverse.vercel.app']
+    });
+  
+    function onRouteChangeComplete() {
+      Fathom.trackPageview();
+    }
+  
+    router.events.on('routeChangeComplete', onRouteChangeComplete);
+  
+    return () => {
+      router.events.off('routeChangeComplete', onRouteChangeComplete);
+    };
+  }, []);
+
+  
   return (
     <div className="relative bg-white">
       <Header />
