0085fa9a080efed9b97dffd34707de2fe64cca7b
Josh Illichmann <josh@studioprisoner.com>
2023-09-27 18:42:56 +1000

    reverting fathom

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import type { AppProps } from "next/app";
-import Fathom from "../components/Fathom";
+import Router from 'next/router';
+import React, { useEffect } from 'react';
+import * as Fathom from 'fathom-client'
 
 import "../styles/global.css";
 import "../styles/scoped-preflight.css";
@@ -7,13 +9,26 @@ import "../styles/scoped-preflight.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
+// Record a pageview when route changes
+Router.events.on('routeChangeComplete', (as, routeProps) => {
+  if (!routeProps.shallow) {
+    Fathom.trackPageview();
+  }
+});
+
 
 
 function MyApp({ Component, pageProps }: AppProps) {
+
+  useEffect(() => {
+    Fathom.load('FATHOM_ID', {
+      includedDomains: ['talentverse.com.au', 'www.talentverse.com.au']
+    });
+  }, []);
   
   return (
     <div className="relative bg-white">
-      <Fathom />
+      
       <Header />
       <main>
           <Component {...pageProps} />
